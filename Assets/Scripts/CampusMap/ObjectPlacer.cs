104179a9c8ff2183cd6ef092f460ebf44d198139
ceciliazurla <100606122+ceciliazurla@users.noreply.github.com>
2024-04-24 10:25:00 +0200

    Added Construction and removing preview and added the possibility to removing object

---
+++ Assets/Scripts/CampusMap/ObjectPlacer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPlacer : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> placedGameObject = new();
+
+    public int PlaceObject(ObjectsDatabaseSo database, int selectedObjectIndex, GameObject prefab, Grid grid, Vector3Int gridPosition)
+    {
+        GameObject newObject = Instantiate(prefab);
+        if (database.objectsData[selectedObjectIndex].Size.x == 1)
+        {
+            newObject.transform.position = grid.GetCellCenterWorld(gridPosition) - new Vector3(0, 0.5f, 0);
+        }
+        else
+        {
+            newObject.transform.position = grid.GetCellCenterWorld(gridPosition) - new Vector3(-0.5f, 0.5f, 0);
+        }
+
+        placedGameObject.Add(newObject);
+        return placedGameObject.Count - 1;
+    }
+
+    internal void RemoveObjectAt(int gameObjectIndex)
+    {
+        if(placedGameObject.Count <= gameObjectIndex || placedGameObject[gameObjectIndex] == null)
+        {
+            return;
+        }
+        Destroy(placedGameObject[gameObjectIndex]);
+        placedGameObject[gameObjectIndex] = null;
+    }
+}
---
+++ Assets/Scripts/CampusMap/PlacementState.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementState : IBuildingState
+{
+    private int selectedObjectIndex = -1;
+    int ID;
+    Grid grid;
+    PreviewSystem previewSystem;
+    ObjectsDatabaseSo database;
+    GridData buildingData;
+    GridData floorData;
+    ObjectPlacer objectPlacer;
+
+    public PlacementState(int iD, Grid grid, PreviewSystem previewSystem, ObjectsDatabaseSo database, GridData floorData ,GridData buildingData, ObjectPlacer objectPlacer)
+    {
+        ID = iD;
+        this.grid = grid;
+        this.previewSystem = previewSystem;
+        this.database = database;
+        this.floorData = floorData;
+        this.buildingData = buildingData;
+        this.objectPlacer = objectPlacer;
+
+        selectedObjectIndex = database.objectsData.FindIndex(data => data.ID == ID);
+        if (selectedObjectIndex > -1)
+        {
+
+            previewSystem.StartShowingPlacementPreview(database.objectsData[selectedObjectIndex].Prefab, database.objectsData[selectedObjectIndex].Size);
+        }
+        else
+        {
+            throw new System.Exception($"No objects with ID {iD}");
+        }
+
+    }
+
+    public void EndState()
+    {
+        previewSystem.StopShowingPreview();
+    }
+
+    public void OnAction(Vector3Int gridPosition)
+    {
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+        if (placementValidity == false)
+            return;
+
+        int index = objectPlacer.PlaceObject(database, selectedObjectIndex, database.objectsData[selectedObjectIndex].Prefab, grid, gridPosition);
+
+
+
+        GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ? floorData : buildingData;
+        selectedData.AddObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size, database.objectsData[selectedObjectIndex].ID, index);
+        previewSystem.UpdatePosition(grid.CellToWorld(gridPosition), false);
+    }
+
+    public bool CheckPlacementValidity(Vector3Int gridPosition, int selectedObjectIndex)
+    {
+        GridData selectedData = database.objectsData[selectedObjectIndex].ID == 0 ? floorData : buildingData;
+        return selectedData.CanPlaceObjectAt(gridPosition, database.objectsData[selectedObjectIndex].Size);
+    }
+
+    public void UpdateState(Vector3Int gridPosition)
+    {
+        bool placementValidity = CheckPlacementValidity(gridPosition, selectedObjectIndex);
+
+        if (database.objectsData[selectedObjectIndex].Size.x == 1)
+        {
+            previewSystem.UpdatePosition(grid.GetCellCenterWorld(gridPosition) - new Vector3(0, 0.5f, 0), placementValidity);
+        }
+        else
+        {
+            previewSystem.UpdatePosition(grid.GetCellCenterWorld(gridPosition) - new Vector3(-0.6f, 0.5f, 0), placementValidity);
+        }
+    }
+}
