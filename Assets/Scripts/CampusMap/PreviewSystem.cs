aa200eae4381cf044e407b00a8902483bdbac0e5
ceciliazurla <100606122+ceciliazurla@users.noreply.github.com>
2024-05-10 12:06:24 +0200

    changed building collision, enlarged grass plane, added functionality to one of the UI buttons

--- Assets/Scripts/CampusMap/PreviewSystem.cs
+++ Assets/Scripts/CampusMap/PreviewSystem.cs
@@ -41,7 +41,9 @@ public class PreviewSystem : MonoBehaviour
     {
         if (size.x > 0 || size.y >0)
         {
-            cellIndicator.transform.localScale = new Vector3(size.x, 1, size.y);
+
+
+            cellIndicator.transform.localScale = new Vector3(size.x + 2, 3, size.y + 2);
             cellIndicatorRenderer.material.mainTextureScale = size;
         }
     }
